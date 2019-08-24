f7fc14568b81692e43e67af6ca7cbd2275470571
egorlw26 <egorlw26@gmail.com>
2019-08-24 15:44:47 +0300

    Changed Google Tests

--- STL_Importer/GoogleTests/test.cpp
+++ STL_Importer/GoogleTests/test.cpp
@@ -3,7 +3,8 @@
 TEST(Expected4When4, NumberTest1) {
 	STL_Importer test;
 	std::vector<STL_Importer::Face> test_vector;
-	test.Import("TestTxtFiles/Expect4Test.txt", test_vector);
+	std::vector<STL_Importer::Point> test_vertexs;
+	test.Import("../TestTxtFiles/Expect4Test.txt", test_vector, test_vertexs);
 	int expected = 4;
 	EXPECT_EQ(expected, test_vector.size());
 }
@@ -11,15 +12,17 @@ TEST(Expected4When4, NumberTest1) {
 TEST(ExpectedTrue, HasNormalTrueTest) {
 	STL_Importer test;
 	std::vector<STL_Importer::Face> test_vector;
-	test.Import("TestTxtFiles/ExpectHasNormalTrueTest.txt", test_vector);
+	std::vector<STL_Importer::Point> test_vertexs;
+	test.Import("../TestTxtFiles/ExpectHasNormalTrueTest.txt", test_vector, test_vertexs);
 	bool expected = true;
 	EXPECT_EQ(expected, test_vector[0].m_hasNormal);
 }
 
-TEST(ExpectedTrue, HasNormalFalseTest) {
+TEST(ExpectedFalse, HasNormalFalseTest) {
 	STL_Importer test;
 	std::vector<STL_Importer::Face> test_vector;
-	test.Import("TestTxtFiles/ExpectHasNormalFalseTest.txt", test_vector);
+	std::vector<STL_Importer::Point> test_vertexs;
+	test.Import("../TestTxtFiles/ExpectHasNormalFalseTest.txt", test_vector, test_vertexs);
 	bool expected = false;
 	EXPECT_EQ(expected, test_vector[0].m_hasNormal);
 }
@@ -27,42 +30,49 @@ TEST(ExpectedTrue, HasNormalFalseTest) {
 TEST(ExpectedThrow, SolidFailTest) {
 	STL_Importer test;
 	std::vector<STL_Importer::Face> test_vector;
-	EXPECT_ANY_THROW(test.Import("TestTxtFiles/ExpectSolidFailTest.txt", test_vector));
+	std::vector<STL_Importer::Point> test_vertexs;
+	EXPECT_ANY_THROW(test.Import("../TestTxtFiles/ExpectSolidFailTest.txt", test_vector, test_vertexs));
 }
 
 TEST(ExpectedThrow, FacetFailTest) {
 	STL_Importer test;
 	std::vector<STL_Importer::Face> test_vector;
-	EXPECT_ANY_THROW(test.Import("TestTxtFiles/ExpectFacetFailTest.txt", test_vector));
+	std::vector<STL_Importer::Point> test_vertexs;
+	EXPECT_ANY_THROW(test.Import("../TestTxtFiles/ExpectFacetFailTest.txt", test_vector, test_vertexs));
 }
 
 TEST(ExpectedThrow, OuterLoopFailTest) {
 	STL_Importer test;
 	std::vector<STL_Importer::Face> test_vector;
-	EXPECT_ANY_THROW(test.Import("TestTxtFiles/ExpectOuterLoopFailTest.txt", test_vector));
+	std::vector<STL_Importer::Point> test_vertexs;
+	EXPECT_ANY_THROW(test.Import("../TestTxtFiles/ExpectOuterLoopFailTest.txt", test_vector, test_vertexs));
 }
 
 TEST(ExpectedThrow, VertexFailTest) {
 	STL_Importer test;
 	std::vector<STL_Importer::Face> test_vector;
-	EXPECT_ANY_THROW(test.Import("TestTxtFiles/ExpectVertexFailTest.txt", test_vector));
+	std::vector<STL_Importer::Point> test_vertexs;
+	EXPECT_ANY_THROW(test.Import("../TestTxtFiles/ExpectVertexFailTest.txt", test_vector, test_vertexs));
 }
 
 TEST(ExpectedThrow, EndLoopFailTest) {
 	STL_Importer test;
 	std::vector<STL_Importer::Face> test_vector;
-	EXPECT_ANY_THROW(test.Import("TestTxtFiles/ExpectEndLoopFailTest.txt", test_vector));
+	std::vector<STL_Importer::Point> test_vertexs;
+	EXPECT_ANY_THROW(test.Import("../TestTxtFiles/ExpectEndLoopFailTest.txt", test_vector, test_vertexs));
 }
 
 TEST(ExpectedThrow, EndFacetFailTest) {
 	STL_Importer test;
 	std::vector<STL_Importer::Face> test_vector;
-	EXPECT_ANY_THROW(test.Import("TestTxtFiles/ExpectEndFacetFailTest.txt", test_vector));
+	std::vector<STL_Importer::Point> test_vertexs;
+	EXPECT_ANY_THROW(test.Import("../TestTxtFiles/ExpectEndFacetFailTest.txt", test_vector, test_vertexs));
 }
 
 TEST(ExpectedThrow, EndSolidFailTest) {
 	STL_Importer test;
 	std::vector<STL_Importer::Face> test_vector;
-	EXPECT_ANY_THROW(test.Import("TestTxtFiles/ExpectEndSolidFailTest.txt", test_vector));
+	std::vector<STL_Importer::Point> test_vertexs;
+	EXPECT_ANY_THROW(test.Import("../TestTxtFiles/ExpectEndSolidFailTest.txt", test_vector, test_vertexs));
 }
 
