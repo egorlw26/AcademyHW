6f2f1f7c56adebbce2499a07420006aea6e776a6
Egor Delikatny <36207304+egorlw26@users.noreply.github.com>
2019-08-29 13:34:11 +0300

    Small updates

--- STL_Importer/Importer/STL_Importer.cpp
+++ STL_Importer/Importer/STL_Importer.cpp
@@ -1,125 +1,83 @@
-#define EXPORT
 
 #include "STL_Importer.h"
 
-STL_Importer::STL_Importer():
-	m_name("")
-{
-}
+STL_Importer::STL_Importer() :
+  m_part_name("")
+  {
+  }
 
-void STL_Importer::Import(const std::string& i_file_path, std::vector<Face>& o_faces, std::vector<Point>& o_vertexs)
-{
-	std::ifstream in(i_file_path);
-	if (in.is_open())
-	{
-		std::string input;
-		in >> input;
-		if (input != "solid")
-			throw std::exception("Invalid file!\n");
-		//STL file can include a name for the object
-		std::getline(in, m_name);
-		m_name = m_name.substr(1);
+/*Importing STL file into std::vector of 'Face's, where 'Face' is nested structure in STL_Importer*/
+void STL_Importer::Import(const std::string& i_utf8_file_path)
+  {
+  std::ifstream in(i_utf8_file_path);
+  if (in.is_open())
+    {
+    std::string input;
+    in >> input;
+    if (input != "solid")
+      throw std::logic_error("Abort. Supposed to read 'solid'!\n");
 
-		//Starting reading faces
-		while (true)
-		{
-			in >> input;
+    //STL file can include a name for the object, it's empty by default
+    std::getline(in, m_part_name);
+    m_part_name = m_part_name.substr(1);
 
-			if (input == "endsolid")
-				break;
-			if(in.eof())
-				throw std::exception("Something wrong while file reading!\n");
+    //Starting reading faces
+    while (true)
+      {
+      in >> input;
 
-			if (input != "facet")
-				throw std::exception("Something wrong while file reading!\n");
-			Face t_face;
-			
-			//Can include normal for face
-			std::getline(in, input);
-			if (input.length() > 1)
-			{
-				t_face.m_hasNormal = true;
-				input = input.substr(1);
-				size_t pos = input.find(' ');
-				std::string temp = input.substr(0, pos);
-				if(temp != "normal")
-					throw std::exception("Something wrong while file reading!\n");
-				input = input.substr(pos + 1);
-				for (size_t i = 0; i < 2; ++i)
-				{
-					pos = input.find(' ');
-					temp = input.substr(0, pos);
-					t_face.m_normal[i] = std::stof(temp);
-					input = input.substr(pos + 1);
-				}
-				t_face.m_normal[2] = std::stof(input);
-			}
+      if (input == "endsolid")
+        break;
 
-			//Reading vertexes
-			std::getline(in, input);
-			if(input != "outer loop" && input != "\touter loop")
-				throw std::exception("Something wrong while file reading!\n");
+      if (in.eof())
+        throw std::logic_error("Abort. File ended incorrectly!\n");
 
-			for (size_t i = 0; i < 3; ++i)
-			{
-				in >> input;
-				if(input != "vertex" && input != "\tvertex")
-					throw std::exception("Something wrong while file reading!\n");
-				std::array<float, 3> vertex;
-				in >> vertex[0] >> vertex[1] >> vertex[2];
-				auto pos = std::find(o_vertexs.begin(), o_vertexs.end(), vertex);
-				if (pos != o_vertexs.end())
-					t_face.m_triangle[i] = pos - o_vertexs.begin();
-				else
-				{
-					o_vertexs.push_back(vertex);
-					t_face.m_triangle[i] = o_vertexs.size() - 1;
-				}
-			}
-			in >> input;
-			if(input != "endloop" && input != "\tendloop")
-				throw std::exception("Something wrong while file reading!\n");
-			in >> input;
-			if(input != "endfacet")
-				throw std::exception("Something wrong while file reading!\n");
-			o_faces.push_back(t_face);
-		}
-	}
-	else
-		throw std::exception("Wrong file path, check it again, please!\n");
-}
+      if (input != "facet")
+        throw std::logic_error("Abort. Supposed to read 'facet'!\n");
+      Face t_face;
 
-/*
-EXAMPLE OF STL FILE
+      //Can include normal for face
+      in >> input;
+      if (input != "outer" && input != "\touter" && input != "normal")
+        throw std::logic_error("Abort. Something wrong after 'facet'!\n");
 
-solid cube_corner
-		  facet normal 0.0 -1.0 0.0
-			outer loop
-			  vertex 0.0 0.0 0.0
-			  vertex 1.0 0.0 0.0
-			  vertex 0.0 0.0 1.0
-			endloop
-		  endfacet
-		  facet normal 0.0 0.0 -1.0
-			outer loop
-			  vertex 0.0 0.0 0.0
-			  vertex 0.0 1.0 0.0
-			  vertex 1.0 0.0 0.0
-			endloop
-		  endfacet
-		  facet normal -1.0 0.0 0.0
-			outer loop
-			  vertex 0.0 0.0 0.0
-			  vertex 0.0 0.0 1.0
-			  vertex 0.0 1.0 0.0
-			endloop
-		  endfacet
-		  facet normal 0.577 0.577 0.577
-			outer loop
-			  vertex 1.0 0.0 0.0
-			  vertex 0.0 1.0 0.0
-			  vertex 0.0 0.0 1.0
-			endloop
-		  endfacet
-		endsolid
-*/
+      if (input == "normal")
+        {
+        t_face.m_has_normal = true;
+        in >> t_face.m_normal[0] >> t_face.m_normal[1] >> t_face.m_normal[2];
+        std::getline(in, input);
+        }
+
+      //Reading vertexes
+      std::getline(in, input);
+      if (input != " loop" && input != "outer loop" && input != "\touter loop")
+        throw std::logic_error("Abort. Supposed to read 'outer loop'!\n");
+
+      for (size_t i = 0; i < 3; ++i)
+        {
+        in >> input;
+        if (input != "vertex" && input != "\tvertex")
+          throw std::logic_error("Abort. Supposed to read 'vertex'!\n");
+        Vertex vertex;
+        in >> vertex[0] >> vertex[1] >> vertex[2];
+        auto pos = std::find(m_vertexes.begin(), m_vertexes.end(), vertex);
+        if (pos != m_vertexes.end())
+          t_face.m_triangle[i] = static_cast<int>(pos - m_vertexes.begin());
+        else
+          {
+          m_vertexes.push_back(vertex);
+          t_face.m_triangle[i] = m_vertexes.size() - 1;
+          }
+        }
+      in >> input;
+      if (input != "endloop" && input != "\tendloop")
+        throw std::logic_error("Abort. Supposed to read 'endloop'!\n");
+      in >> input;
+      if (input != "endfacet")
+        throw std::logic_error("Abort. Supposed to read 'endfacet'!\n");
+      m_faces.push_back(t_face);
+      }
+    }
+  else
+    throw std::logic_error("Wrong file path, check it again, please!\n");
+  }
--- STL_Importer/Importer/STL_Importer.h
+++ STL_Importer/Importer/STL_Importer.h
@@ -1,38 +1,49 @@
 #pragma once
 
-#ifdef EXPORT
-#define STL_Importer_API __declspec(dllimport)
-#else
+#ifdef STL_Importer_API_EXPORT
 #define STL_Importer_API __declspec(dllexport)
-#endif //EXPORT
+#else
+#define STL_Importer_API __declspec(dllimport)
+#endif //STL_Importer_API_EXPORT
 
-#include <string>
-#include <fstream>
-#include <vector>
-#include <array>
 #include <algorithm>
+#include <array>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-class STL_Importer_API STL_Importer
+class STL_Importer
 {
 public:
 
-	using Point = std::array<float, 3>;
-	using Triangle = std::array<int, 3>;
+  using Vertex = std::array<double, 3>;
+  using Triangle = std::array<int32_t, 3>;
 
-	struct Face
-	{
-		bool m_hasNormal = false;
-		Point m_normal = { 0, 0, 0 };
-		Triangle m_triangle;
-	};
-	STL_Importer();
-	STL_Importer(const STL_Importer&) = delete;
-	STL_Importer(STL_Importer&&) = delete;
-	STL_Importer& operator = (const STL_Importer&) = delete;
-	STL_Importer& operator = (STL_Importer&&) = delete;
-	void Import(const std::string& i_file_path, std::vector<Face>& o_faces, std::vector<Point>& o_vertexs);	
-	~STL_Importer() = default;
+  struct Face
+  {
+    bool m_has_normal = false;
+    Vertex m_normal = { 0, 0, 0 };
+    Triangle m_triangle;
+  };
+
+  STL_Importer_API STL_Importer();
+
+  STL_Importer_API void Import(const std::string& i_utf8_file_path);
+
+  STL_Importer_API std::vector<Face>& GetFaces() { return m_faces; };
+  STL_Importer_API std::vector<Vertex>& GetVertexes() { return m_vertexes; };
+
+  STL_Importer(const STL_Importer& other) = delete;
+  STL_Importer(STL_Importer&& other) = delete;
+  STL_Importer& operator = (const STL_Importer& other) = delete;
+  STL_Importer& operator = (STL_Importer&& other) = delete;
+
+  STL_Importer_API ~STL_Importer() = default;
+
 private:
-	std::string m_name;
+  std::string m_part_name;
+  std::vector<Face> m_faces;
+  std::vector<Vertex> m_vertexes;
 };
 
